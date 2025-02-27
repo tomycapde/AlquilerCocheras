239b71b69bbcc129724910570587603926d1594b
rachidesper <rachidesper290@gmail.com>
2025-02-27 00:01:12 -0300

    Falta hacer administracion de los users de la DB

--- src/main/java/com/example/alquilercocheras/database/UsersDAO.java
+++ src/main/java/com/example/alquilercocheras/database/UsersDAO.java
@@ -1,4 +1,29 @@
 package com.example.alquilercocheras.database;
 
+import com.example.alquilercocheras.models.User;
+
+import java.sql.PreparedStatement;
+
 public class UsersDAO {
+
+/*
+
+    public static void addNewUser(User user){
+        try {
+            Connection connection = Database.getConnection();
+            String query = "INSERT INTO users (nombre, password,idTipoUsuario) VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, user.getUserTypeId());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDAO.class.getName());
+
+    }
+}
+
+*/
 }
